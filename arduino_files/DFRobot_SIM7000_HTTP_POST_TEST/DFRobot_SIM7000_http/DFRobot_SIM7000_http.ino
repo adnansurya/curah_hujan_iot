ee9010cc0d394dd52a512574b1206c98cb3f6384
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-12-27 16:28:54 +0800

    separation function

--- arduino_files/DFRobot_SIM7000_HTTP_POST_TEST/DFRobot_SIM7000_http/DFRobot_SIM7000_http.ino
+++ arduino_files/DFRobot_SIM7000_HTTP_POST_TEST/DFRobot_SIM7000_http/DFRobot_SIM7000_http.ino
@@ -20,7 +20,7 @@
 SoftwareSerial     mySerial(PIN_RX,PIN_TX);
 DFRobot_SIM7000    sim7000;
 
-String waktu, sen1, sen2, sen3, sen4, sen5;
+String data, waktu, sen1, sen2, sen3, sen4, sen5;
 
 void setup(){
     int signalStrength,dataNum;
@@ -140,4 +140,25 @@ void setup(){
 
 void loop() {
     delay(1000);
+     while (Serial.available() > 0) {
+      data = Serial.readStringUntil('\n');
+      data.trim();
+       waktu = getValue(data, '|', 2);
+     }
+}
+
+
+String getValue(String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
